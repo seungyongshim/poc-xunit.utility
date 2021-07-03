7ba546c5ef1415f1e73a6673664d6f6e02d493d1
SeungYongShim <syshim@me.com>
2021-07-03 10:49:28 +0900

    동작함

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,23 +1,81 @@
-using Xunit;
+using System;
+using System.Threading;
+using Xunit.Runners;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static readonly object consoleLock = new object();
+
+        // Use an event to know when we're done
+        private static readonly ManualResetEvent finished = new ManualResetEvent(false);
+
+        // Start out assuming success; we'll set this to 1 if we get a failed test
+        private static int result = 0;
+
+        private static int Main(string[] args)
         {
-            var xunit = new XunitFrontController(AppDomainSupport.IfAvailable, "Sample.Tests.dll");
-            var discoverySink = new TestDiscoverySink();
+            using var runner = AssemblyRunner.WithoutAppDomain("Sample.Tests.dll");
 
-            xunit.Find(true, discoverySink, TestFrameworkOptions.ForDiscovery());
+            runner.OnDiscoveryComplete = OnDiscoveryComplete;
+            runner.OnExecutionComplete = OnExecutionComplete;
+            runner.OnTestFailed = OnTestFailed;
+            runner.OnTestSkipped = OnTestSkipped;
 
-            discoverySink.Finished.WaitOne();
+            Console.WriteLine("Discovering...");
+            runner.Start();
 
-            var cases = discoverySink.TestCases;
+            finished.WaitOne();
+            finished.Dispose();
 
-            var messageSink = new TestMessageSink();
+            return result;
+        }
 
+        private static void OnDiscoveryComplete(DiscoveryCompleteInfo info)
+        {
+            lock (consoleLock)
+            {
+                Console.WriteLine($"Running {info.TestCasesToRun} of {info.TestCasesDiscovered} tests...");
+            }
+        }
 
+        private static void OnExecutionComplete(ExecutionCompleteInfo info)
+        {
+            lock (consoleLock)
+            {
+                Console.WriteLine($"Finished: {info.TotalTests} tests in {Math.Round(info.ExecutionTime, 3)}s ({info.TestsFailed} failed, {info.TestsSkipped} skipped)");
+            }
+
+            finished.Set();
+        }
+
+        private static void OnTestFailed(TestFailedInfo info)
+        {
+            lock (consoleLock)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("[FAIL] {0}: {1}", info.TestDisplayName, info.ExceptionMessage);
+                if (info.ExceptionStackTrace != null)
+                {
+                    Console.WriteLine(info.ExceptionStackTrace);
+                }
+
+                Console.ResetColor();
+            }
+
+            result = 1;
+        }
+
+        private static void OnTestSkipped(TestSkippedInfo info)
+        {
+            lock (consoleLock)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("[SKIP] {0}: {1}", info.TestDisplayName, info.SkipReason);
+                Console.ResetColor();
+            }
         }
     }
 }
